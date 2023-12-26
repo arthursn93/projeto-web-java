de8e110707a58bc26e9a5e0737c16632505a20a5
Arthur Nunes <arthur_sn_@hotmail.com>
2023-12-26 14:46:27 -0300

    Estruturando infra do projeto v1

---
+++ meu-projeto-web/src/main/java/br/com/projeto/web/meuprojetoweb/MeuProjetoWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.web.meuprojetoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoWebApplication.class, args);
+	}
+
+}
---
+++ meu-projeto-web/src/test/java/br/com/projeto/web/meuprojetoweb/MeuProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.web.meuprojetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
